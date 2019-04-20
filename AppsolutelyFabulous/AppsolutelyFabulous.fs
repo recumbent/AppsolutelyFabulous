3a3864533a0288f866ba15b3964f36c83c0b25e0
James Murphy <james@recumbent.co.uk>
2019-04-20 16:27:02 +0100

    Add some interactivity
    Add data entry and trigger update of label from change

--- AppsolutelyFabulous/AppsolutelyFabulous.fs
+++ AppsolutelyFabulous/AppsolutelyFabulous.fs
@@ -8,24 +8,31 @@ open Xamarin.Forms
 
 module App = 
     type Model = 
-      { name : string
+      { Name : string
       }
 
     type Msg = 
         | NameChanged of string
 
-    let initModel = { name = "World!" }
+    let initModel = { Name = "World!" }
 
     let init () = initModel, Cmd.none
 
-    let update (msg: Msg) model =
-        model, Cmd.none
+    let update msg model =
+        match msg with
+        | NameChanged newName -> { model with Name = newName }, Cmd.none
 
     let view (model: Model) dispatch =
         View.ContentPage(
-          content = View.StackLayout(padding = 20.0, verticalOptions = LayoutOptions.Center,
-            children = [ 
-                View.Label(text = sprintf "Hello %s" model.name, horizontalOptions = LayoutOptions.Start, horizontalTextAlignment=TextAlignment.Start)
+            content = View.StackLayout(
+                padding = 20.0,
+                verticalOptions = LayoutOptions.Center,
+                children = [ 
+                    View.Label(sprintf "Hello %s" model.Name, horizontalOptions = LayoutOptions.Start, horizontalTextAlignment=TextAlignment.Start)
+                    View.Entry(
+                        text = model.Name,
+                        textChanged = (fun args -> dispatch (NameChanged(args.NewTextValue)))
+                    )
             ]))
 
     // Note, this declaration is needed if you enable LiveUpdate
